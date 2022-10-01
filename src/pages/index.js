ae76a9d589a03a3247117a7b96ab33a1bae4e81b
Axel358 <edgar.serrano.9803@gmail.com>
2022-10-01 14:52:10 -0400

    Start clening up

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head';
 import React from "react";
 import { Box, Container, Grid } from '@mui/material';
 import { Budget } from '../components/dashboard/budget';
-import { LatestOrders } from '../components/dashboard/latest-orders';
-import { LatestProducts } from '../components/dashboard/latest-products';
 import { Sales } from '../components/dashboard/sales';
 import { TasksProgress } from '../components/dashboard/tasks-progress';
 import { TotalCustomers } from '../components/dashboard/total-customers';
@@ -95,24 +93,6 @@ const Dashboard = () => {
               xs={12}
             >
               <TrafficByDevice sx={{ height: '100%' }} />
-            </Grid>
-            <Grid
-              item
-              lg={4}
-              md={6}
-              xl={3}
-              xs={12}
-            >
-              <LatestProducts sx={{ height: '100%' }} />
-            </Grid>
-            <Grid
-              item
-              lg={8}
-              md={12}
-              xl={9}
-              xs={12}
-            >
-              <LatestOrders />
             </Grid>
           </Grid>
         </Container>
