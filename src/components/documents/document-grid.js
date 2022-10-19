86c4d41d754d40bc4f31e50497332cdc00fcbfa4
Axel358 <edgar.serrano.9803@gmail.com>
2022-10-19 15:29:05 -0400

    Start work on document grid

---
+++ src/components/documents/document-grid.js
@@ -0,0 +1,46 @@
+import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material';
+import { PopupMenu } from '../popup-menu';
+import { DocumentIcon } from 'src/icons/document';
+
+export const DocumentGrid = ({documents, handleAction}) => {
+  return (
+    <Box
+      container
+      spacing={3}
+      sx={{
+        justifyContent: 'space-between',
+        display: 'flex',
+        wrap: 1
+      }}
+    >
+       {documents.slice(0, documents.length).map((document) => (
+      <Card>
+      <Box
+      sx={{
+        alignItems: 'center',
+        minWidth: 280,
+        display: 'flex',
+        p: 1.5,
+      }}
+      >
+      <DocumentIcon
+        sx={{ color: "secondary.main" }}
+        fontSize="large" />
+      <Typography
+        sx={{ mx: 1, flexGrow: 1 }}
+        color="textPrimary"
+        variant="body1"
+      >
+       {document.name}
+      </Typography>
+      <PopupMenu
+         onAction={'document'}
+         handleAction={handleAction}
+         instance={document.id}
+      />
+    </Box>
+    </Card>
+    ))}
+    </Box>
+  );
+};
---
+++ src/components/documents/new-simple-doc-dialog.js
@@ -0,0 +1,71 @@
+import { Dialog, Button, TextField, Box, DialogTitle, FormControl, InputLabel, MenuItem } from '@mui/material';
+import { useState } from 'react';
+import { addDocument} from '../../utils/requests';
+import { useData } from '../../utils/hooks';
+
+export const NewSDocumentDialog = ({ open, handleClose, loadData, onAction, ...rest }) => {
+  const [docName, setDocName] = useState('')
+  const [file, setFile] = useState()
+  const [errors, setErrors] = useData({});
+
+  const saveDocument = async () => {
+    let data = new FormData();
+      data.append("file", file, docName);
+      data.append("name", docName);
+      addDocument(data, "/documents/").then((data) => {
+        handleClose();
+        loadData();
+      })
+        .catch((error) => {
+          //setErrors(error.response.data)
+          console.log(error.response.data)
+        });
+  };
+
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      maxWidth='xs'
+      fullWidth={true}
+
+    >
+      <DialogTitle>{onAction === 'new_document' ? "Nuevo documento" : "Editar documento"}</DialogTitle>
+      <Box
+        sx={{ px: 2, mx: 2, display: "flex", flexDirection: "column" }}
+      >
+        <TextField
+          sx={{ mt: 1 }}
+          onChange={(event) => setDocName(event.target.value)}
+          label="Nombre"
+          variant="standard" />
+        <Button
+          sx={{ mt: 1 }}
+          variant="contained"
+        >
+          <input
+            type="file"
+            name="file_url"
+            onChange={(event) => setFile(event.target.files[0])}
+          />
+        </Button>
+        <Box
+          sx={{ pt: 2, display: "flex", justifyContent: "right" }}
+        >
+          <Button
+            onClick={handleClose}
+          >
+            Cancelar
+          </Button>
+          <Button
+            onClick={saveDocument}
+          >
+            Guardar
+          </Button>
+        </Box>
+      </Box>
+    </Dialog>
+  );
+
+};
